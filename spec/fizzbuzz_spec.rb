ece9dc9a544f8286f9ac0be1c5bf44a6c6258330
AlfieDarko <me@alfiedarko.co.uk>
2018-02-17 21:12:11 +0000

    1st passing method and 2nd failing test written

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,7 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it "returns 'fizz' if number passed is divisible by 3 with no remainders" do
+    expect(fizzbuzz(9)).to eq 'fizz'
+  end
 end
